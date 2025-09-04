3ac184973c02d549bc19157fa33a225277e4a92e
Lucas-Sperati <lucassperati@gmail.com>
2025-09-04 13:52:24 -0400

    added work for miner.sh. forgot to upload it earlier, but as of right now it combines the numbers to each word and then hashes it, and prints out each one

--- miner/miner.sh
+++ miner/miner.sh
@@ -4,13 +4,24 @@
 echo "Starting this script (hopefully it is executable with chmod a+x ./miner.sh)"
 
 VARIABLE=some_string
+VARIABLE=hashed_string
+VARIABLE=random_number
 
 echo $VARIABLE
 
 ## Prints all words in provided dictionary
 ## (you might want to find a bigger dictionary)
 for i in $(cat ../data/dictionary); do
-  printf $i
+  printf $i 
+  printf "\n"
+
+  for j in $(seq 100 105); do
+  	some_string="$j $i"
+	printf $some_string
+  	hashed_string=$(printf "$some_string" | sha256sum)
+  	echo "$hashed_string"  
+
+done
 done
 
 
@@ -19,4 +30,4 @@ for i in $(seq 100 105); do
   printf $i
 done
 
-
+echo "Script Done"
